162b8ec6800983995458bd15a760483ce182b0a2
Junqi Hong <harryhong104@gmail.com>
2023-01-16 22:30:00 -0500

    Initial Commit

---
+++ app/src/main/java/algonquin/cst2335/ID041012545/MainActivity.java
@@ -0,0 +1,19 @@
+package algonquin.cst2335.ID041012545;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override  //This starts the application
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //loads buttons / text on screen
+        setContentView(R.layout.activity_main);
+                        //R means res, layout is the folder, activity-main is the file
+
+
+    }
+}
