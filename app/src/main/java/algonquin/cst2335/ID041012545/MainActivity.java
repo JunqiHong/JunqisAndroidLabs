e16571045704908b861f1059dd4f913fe5f19b9c
Junqi Hong <harryhong104@gmail.com>
2023-01-24 23:00:40 -0500

    Commit 6 page4-unfinished

--- app/src/main/java/algonquin/cst2335/ID041012545/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/ID041012545/MainActivity.java
@@ -1,19 +1,64 @@
 package algonquin.cst2335.ID041012545;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import algonquin.cst2335.ID041012545.data.MainActivityViewModel;
+import algonquin.cst2335.ID041012545.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private ActivityMainBinding binding;
+
+    MainActivityViewModel viewModel;
 
     @Override  //This starts the application
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater() ); // produces layout inflater
+
+
+        binding.textview.setText(viewModel.editString);
+        binding.button.setText(viewModel.editString);
+
+
+
         //loads buttons / text on screen
-        setContentView(R.layout.activity_main);
-                        //R means res, layout is the folder, activity-main is the file
+        setContentView(binding.getRoot()); //load stuff on screen
+        //R means res, layout is the folder, activity-main is the file
 
 
-    }
-}
+
+        binding.textview.setText(viewModel.editString);
+
+        binding.button.setOnClickListener((veeew) -> {
+
+        });
+
+        binding.button.setOnClickListener(v -> {
+            viewModel.editString="You Clicked the button";
+
+            binding.textview.setText(viewModel.editString);
+            binding.button.setText(viewModel.editString);
+
+//            viewModel.editString=binding.editString.getText().toString();
+//            binding.textview.setText("You clicked the button");
+        });
+    };
+
+
+
+};
+
+
+
+
---
+++ app/src/main/java/algonquin/cst2335/ID041012545/data/MainActivityViewModel.java
@@ -0,0 +1,7 @@
+package algonquin.cst2335.ID041012545.data;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainActivityViewModel extends ViewModel {
+    public String editString;
+}
