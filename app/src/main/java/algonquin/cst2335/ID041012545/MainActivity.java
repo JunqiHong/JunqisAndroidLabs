146e68ff73ad0337dcb0ef12be032b0fb5094b49
Junqi Hong <harryhong104@gmail.com>
2023-02-08 20:07:40 -0500

    Commit Lab3 version2

--- app/src/main/java/algonquin/cst2335/ID041012545/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/ID041012545/MainActivity.java
@@ -3,18 +3,32 @@ package algonquin.cst2335.ID041012545;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override  //This starts the application
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
 
         //loads buttons / text on screen
         setContentView(R.layout.activity_main);
-                        //R means res, layout is the folder, activity-main is the file
+        //R means res, layout is the folder, activity-main is the file
+        ImageView imgView = findViewById(R.id.imageView);
+        Switch sw = findViewById(R.id.switch1);
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
+            imgView = findViewById(R.id.imageView);
+            sw = findViewById(R.id.switch1);
 
+            sw.setOnCheckedChangeListener((btn, isChecked) -> {
+
+            });
         }
 
 
