d43da1dc3f95d073365109900a83e042d807764d
Junqi Hong <harryhong104@gmail.com>
2023-02-08 21:07:33 -0500

    Commit Lab3-4

--- app/src/main/java/algonquin/cst2335/ID041012545/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/ID041012545/MainActivity.java
@@ -3,34 +3,44 @@ package algonquin.cst2335.ID041012545;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
+    Switch switch1;
+    ImageView imageView;
+
+
     @Override  //This starts the application
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        super.onCreate(savedInstanceState);
 
-        //loads buttons / text on screen
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //R means res, layout is the folder, activity-main is the file
-        ImageView imgView = findViewById(R.id.imageView);
-        Switch sw = findViewById(R.id.switch1);
-        @Override
-        protected void onCreate (Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
 
-            imgView = findViewById(R.id.imageView);
-            sw = findViewById(R.id.switch1);
+        switch1 = (Switch) findViewById(R.id.switch1);
+        switch1.setOnCheckedChangeListener((btn, isChecked) ->{
+            if (isChecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
 
-            sw.setOnCheckedChangeListener((btn, isChecked) -> {
+                imageView.startAnimation(rotate);
+            }
+            else {
+                imageView.clearAnimation();
+            }
 
-            });
-        }
+        });
+    }
+
+
 
 
     }
-}
