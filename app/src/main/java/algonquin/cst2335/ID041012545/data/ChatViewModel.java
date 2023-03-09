88c02ebd38f968240ac0bf515ef1483fd3dc842a
Junqi Hong <harryhong104@gmail.com>
2023-03-08 20:33:11 -0500

    Week6-1

---
+++ app/src/main/java/algonquin/cst2335/ID041012545/data/ChatViewModel.java
@@ -0,0 +1,9 @@
+package algonquin.cst2335.ID041012545.data;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+
+public class ChatViewModel extends ViewModel {
+    public MutableLiveData<ArrayList<ChatMessage>> messages = new MutableLiveData<>();
+}
